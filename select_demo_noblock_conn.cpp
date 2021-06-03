9958a0e4177a6993162fe1bf53f682eea1668ab6
zh <im.zengh@gmail.com>
2021-06-03 23:20:25 +0800

    update net.h and net.cpp

---
+++ select_demo_noblock_conn.cpp
@@ -0,0 +1,26 @@
+//
+// Created by zz on 2021/6/3.
+//
+
+#include <fcntl.h>
+#include <iostream>
+#include "./pkg/net/net.h"
+
+using std::string;
+
+int set_no_blocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+int unblock_connect(string &ip, string &port, int time) {
+    int ret = 0;
+    Dial dial("tcp");
+    // 将 dial 的 sockfd 设置为非阻塞
+    int fd = set_no_blocking(dial.get_sockfd());
+
+    Conn conn = dial.Connect(ip, port);
+
+}
